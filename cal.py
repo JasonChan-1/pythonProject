6166ec2cf0caa2b0cc88ade3ee58cf36f433bae4
JCwin <1922689452@qq.com>
2023-03-06 22:08:54 +0800

    JCwin

---
+++ cal.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy as sp
+a=np.array([1,2])
+print("求和",np.sum(a))
+print("平方和",np.sum( np.square(a) ))
+print("平均值",np.average(a))
+print("样本方差",np.var(a,ddof=1))
+print("总体方差",np.var(a,ddof=0))
+
+
+
